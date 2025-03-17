11fe798d6f10059858c389f7a79336b8bf3e1047
Bhaarathan Omarsangar <bhaarathan@192.168.1.16>
2025-03-17 19:43:14 +0800

    ASS-PM

--- cloud_aws/iam_role/iot/main.tf
+++ cloud_aws/iam_role/iot/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 resource "aws_iam_role" "iot_role" {
   name = "IoTCoreLambdaTriggerRole"
 
