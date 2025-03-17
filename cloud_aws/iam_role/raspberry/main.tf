61927aa3d1402168ef93e6d174deb77a37afde1d
Bhaarathan Omarsangar <bhaarathan@192.168.1.16>
2025-03-17 19:48:47 +0800

    ASS-PM

--- cloud_aws/iam_role/raspberry/main.tf
+++ cloud_aws/iam_role/raspberry/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 resource "aws_iam_role" "raspberry_pi_role" {
   name = "RaspberryPiRole"
 
